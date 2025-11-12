a378b44afcc7a3cc6c5fd55b759377155b35173b
Shivathmika20 <yelurishivathmika@gmail.com>
2025-11-12 18:20:26 +0530

    feat: enhanced WebSocket server with room creation and user joining functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer, WebSocket } from 'ws';
-
+import { randomUUID } from 'crypto';
 const wss = new WebSocketServer({ port: 8080 });
 
 interface UserInfo{
@@ -14,29 +14,42 @@ wss.on('connection',(socket)=>{
     
     
     console.log('Client connected',allSockets.length);
-    socket.send('Welcome to the server');
+    
 
     socket.on('message',(message)=>{
         const data=JSON.parse(message.toString());
         console.log(data)
         // console.log(typeof data)
-        if(data.type==='join'){  //evry user coming in the room will be added to the allSockets array
-            allSockets.push(
-                {
-                    socket,
-                    room:data.payload.roomId
-                }
-            )
+
+        //create a room
+        if (data.type === 'create-room') {
+            const newRoomId = randomUUID().slice(0, 6); // short random ID
+            allSockets.push({ socket, room: newRoomId });
+            socket.send(JSON.stringify({ type: 'room-created', payload: { roomId: newRoomId } })); //send the room id to the client
+            console.log(`Room created: ${newRoomId}`);
+        }
+
+        //join a room exisiting one
+        if(data.type==='join')
+        {  //evry user coming in the room will be added to the allSockets array
+            const { roomId } = data.payload;
+            allSockets.push({ socket, room: roomId });
+            socket.send(JSON.stringify({ type: 'joined', payload: { roomId } })); //send the room id to the client
+            console.log(`User joined room: ${roomId}`);
         }
         console.log(allSockets)
 
+        //send a message to the room
         if(data.type==='chat'){  //evry user sending a message will be broadcast to the other users in the same room
             const currentUserRoom=allSockets.find(s=>s.socket===socket)?.room;
-            allSockets.forEach(s=>{
-                if(s.room===currentUserRoom){
-                    s.socket.send(data.payload.message)
-                }
-            })
+            const messageText = data.payload?.message;
+            if (messageText && currentUserRoom) {
+                allSockets.forEach(s=>{
+                    if(s.room===currentUserRoom){
+                        s.socket.send(JSON.stringify({ type: 'chat-message', payload: { message: messageText } }))
+                    }
+                })
+            }
         }
        
      
