817481f979b2646dd815b9558fb39ffd03c92e0a
Shivathmika20 <yelurishivathmika@gmail.com>
2025-11-13 12:58:18 +0530

    feat: added username handling

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const wss = new WebSocketServer({ port: 8080 });
 interface UserInfo{
     socket:WebSocket;
     room:string;
+    username:string;
 }
 
 let allSockets:UserInfo[]=[];
@@ -24,7 +25,7 @@ wss.on('connection',(socket)=>{
         //create a room
         if (data.type === 'create-room') {
             const newRoomId = randomUUID().slice(0, 6); // short random ID
-            allSockets.push({ socket, room: newRoomId });
+            allSockets.push({ socket, room: newRoomId ,username:'Host'});
             socket.send(JSON.stringify({ type: 'room-created', payload: { roomId: newRoomId } })); //send the room id to the client
             console.log(`Room created: ${newRoomId}`);
         }
@@ -33,7 +34,7 @@ wss.on('connection',(socket)=>{
         if(data.type==='join')
         {  //evry user coming in the room will be added to the allSockets array
             const { roomId } = data.payload;
-            allSockets.push({ socket, room: roomId });
+            allSockets.push({ socket, room: roomId ,username:data.payload.username});
             socket.send(JSON.stringify({ type: 'joined', payload: { roomId } })); //send the room id to the client
             console.log(`User joined room: ${roomId}`);
         }
@@ -41,12 +42,14 @@ wss.on('connection',(socket)=>{
 
         //send a message to the room
         if(data.type==='chat'){  //evry user sending a message will be broadcast to the other users in the same room
-            const currentUserRoom=allSockets.find(s=>s.socket===socket)?.room;
+            const currentUser = allSockets.find((s) => s.socket === socket);
+            const currentUserRoom = currentUser?.room;
+            const currentUsername = currentUser?.username;
             const messageText = data.payload?.message;
             if (messageText && currentUserRoom) {
                 allSockets.forEach(s=>{
                     if(s.room===currentUserRoom){
-                        s.socket.send(JSON.stringify({ type: 'chat-message', payload: { message: messageText } }))
+                        s.socket.send(JSON.stringify({ type: 'chat-message', payload: { message: messageText, username: currentUsername } }))
                     }
                 })
             }
@@ -56,9 +59,28 @@ wss.on('connection',(socket)=>{
     })
 
    
-    socket.on('close',()=>{
-        allSockets=allSockets.filter(s=>s.socket!==socket);
-        console.log('Client disconnected',allSockets.length);
-    })
+    socket.on('close', () => {
+        const disconnectedUser = allSockets.find((s) => s.socket === socket);
+        if (disconnectedUser) {
+          const { room, username } = disconnectedUser;
+    
+          // Remove from list
+          allSockets = allSockets.filter((s) => s.socket !== socket);
+    
+          // Notify others in same room
+          allSockets.forEach((s) => {
+            if (s.room === room) {
+              s.socket.send(
+                JSON.stringify({
+                  type: 'user-left',
+                  payload: { username },
+                })
+              );
+            }
+          });
+    
+          console.log(`User ${username} left room ${room}`);
+        }
+      }); 
     
 });
